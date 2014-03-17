da7475aac154ddfe7b432fc090d9a9eb288951d4
jiman <jiman.park@nhn.com>
2014-03-17 15:06:08 +0900

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Naver on 14. 3. 4.
+ *
+ * Author : UIT Development Gman.Park
+ *
+ * Simple Socket IO Chat
+ */
+
+var app = require('express')(), server = require('http').createServer(app), io = require('socket.io').listen(server);
+var port = Number(process.env.PORT || 5000);
+
+server.listen(port);
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+
+//make connection
+io.sockets.on('connection', function(socket) {
+	
+	socket.on('data-changed', function(data) {
+		socket.broadcast.emit('data-changed',{
+			data: data.data
+		});
+	});
+
+});
